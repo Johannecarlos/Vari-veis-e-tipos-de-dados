afca5d85824bc6ee0bce55e15fc535942c8870e7
Johanne Carlos <94209719+Johannecarlos@users.noreply.github.com>
2023-01-05 13:44:34 -0300

    Variáveis e tipos de dados

---
+++ index.js
@@ -0,0 +1,23 @@
+// Variáveis e tipos de dados
+
+/*
+nome = Johanne
+idade = 18
+
+Inteiros
+2.000, -8
+
+Reais
+1.23, -2.3, 60.5, -100,23
+
+Caracteres 
+'Johanne', 'Olha que legal essa foto que tirei',
+
+Lógicos
+true - verdadeiro, false - falso
+1 - verdadeiro, 0 - falso
+
+*/
+
+var nome = 'Johanne';
+console.log(nome);
